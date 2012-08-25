bed907c915ba9c2c3a34dc790d015ad15c7b0241
caujka <asheremet@gmail.com>
2012-08-25 20:53:22 +0300

    Added image load dialog

--- ravlyk.py
+++ ravlyk.py
@@ -14,6 +14,8 @@ except:
 
 class Ravlyk:
     wTree = None
+    window_main=None
+    window_load_images = None
 
     def __init__( self ):
         filename = "main_window.glade"
@@ -21,12 +23,29 @@ class Ravlyk:
         builder.add_from_file(filename)
         builder.connect_signals(self)
 
-        window = builder.get_object('window_main')
-        window.set_title('Ravlyk')
-        window.set_size_request(1000, 500)
-        window.show_all()
+        self.window_main = builder.get_object('window_main')
+        self.window_main.set_title('Ravlyk')
+        self.window_main.set_size_request(1000, 500)
+        self.window_main.show_all()
 
         gtk.main()
+
+    def load_images(self, data):
+        img_load_dialog = gtk.FileChooserDialog (title="Load images",
+                              parent=self.window_main,
+                              action=gtk.FILE_CHOOSER_ACTION_OPEN,
+                              buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+        img_load_dialog.set_select_multiple(True)
+
+        response = img_load_dialog.run()
+
+        if response == gtk.RESPONSE_OK:
+            filenames = img_load_dialog.get_filenames()
+            for file in filenames:
+                print ('%s selected' % (file,))
+        elif response == gtk.RESPONSE_CANCEL:
+            print ('Closed, no files selected')
+        img_load_dialog.destroy()
 
     def quit(self, widget):
         sys.exit(0)
