85e867bb2115dc391026e12948f0ef6dcec03ce2
caujka <asheremet@gmail.com>
2012-08-26 11:27:46 +0300

    added affine transformation matrix calculation

--- whichtransform.py
+++ whichtransform.py
@@ -10,7 +10,15 @@ def _distance(p1, p2):
     return _length(dx, dy)
 
 def _length(x,y):
-    return math.sqrt(x*x+y*y)    
+    return math.sqrt(x*x+y*y)
+
+def _cosa(x1, y1, x2, y2):
+    l1 = _length(x1, y1)
+    l2 = _length(x2, y2)
+    if l1==0 or l2==0:
+        return 1
+    return (x1*x2 + y1*y2) / l1 / l2
+
 
 class WhichTransform:
     """ Gets source two pairs of points: where they are
@@ -40,19 +48,21 @@ class WhichTransform:
         dy_src = self.y2_src - self.y1_src
         dx_dst = self.x2_dst - self.x1_dst
         dy_dst = self.y2_dst - self.y1_dst
-
-        l_src = _length(dx_src, dy_src)
-        l_dst = _length(dx_dst, dy_dst)
-
-        if l_dst == l_src:
-            return 0
-
-        return math.degrees(math.acos(
-            (dx_src*dx_dst + dy_src*dy_dst) / l_dst / l_src
-            ))
-
+        return math.degrees(math.acos(_cosa(dx_src, dy_src, dx_dst, dy_dst)))
 
     def translation(self):
         dx = self.x1_dst - self.x1_src
         dy = self.y1_dst - self.y1_src
         return (dx, dy)
+
+    def matrix(self):
+        dx_src = self.x2_src - self.x1_src
+        dy_src = self.y2_src - self.y1_src
+        dx_dst = self.x2_dst - self.x1_dst
+        dy_dst = self.y2_dst - self.y1_dst
+        cosa = _cosa(dx_src, dy_src, dx_dst, dy_dst)
+        sina = math.sqrt(1-cosa*cosa)
+        (dxx, dyy) = self.translation()
+        s = self.scale()
+
+        return (s*cosa, sina, dxx, -sina, s*cosa, dyy)
